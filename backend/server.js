94e0b4f1096b36423abd0f2165f740ab49f46a8d
turrandott <tarzhakova@gmail.com>
2021-05-06 19:30:31 +0200

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    })
+}
 
 app.use('/api/v1/restaurants', restaurants)
 app.use('*', (req, res) => res.status(404).json({ error: 'not found'}))
